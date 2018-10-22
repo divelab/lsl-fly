1885a7e14d010355c7709b2f2b5e0de6a3704e08
LeiCaiwsu <472243936@qq.com>
2018-10-22 15:09:19 -0500

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -80,14 +80,13 @@ class ResNet(nn.Module):
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
-        l1 = out
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out, l1
+        return out
 
 
 def ResNet18():
